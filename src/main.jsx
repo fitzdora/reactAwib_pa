10872ffb92df861ae668f9cf11a9dbada09e6d17
fitzdora <33418972+fitzdora@users.noreply.github.com>
2024-02-04 16:56:58 +0000

    understanding Material MUI and setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+//import App from './App.jsx'
+import App from './AppMUI';
+//import '../node_modules/bootstrap/dist/css/bootstrap.css'
 //import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
