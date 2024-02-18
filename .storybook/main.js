1baeb225826534ba82c591808cd662f71b736513
fitzdora <33418972+fitzdora@users.noreply.github.com>
2024-02-18 15:20:01 +0000

    Bug fix updated Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,11 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  //stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  stories:["../src/components/**/*.stories.@(js|jsx)"],
-  //staticDirs: ["../public"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm",
-    "@storybook/addon-a11y"
   ],
   framework: {
     name: "@storybook/react-vite",
