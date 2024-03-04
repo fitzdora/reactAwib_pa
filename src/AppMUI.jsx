c02857ab76096e228726d4f3068f09894f5cc84d
fitzdora <33418972+fitzdora@users.noreply.github.com>
2024-03-04 14:52:36 +0000

    moving genderselection to components

--- src/AppMUI.jsx
+++ src/AppMUI.jsx
@@ -1,49 +1,23 @@
 import React from "react";
-import Gender from "./components/genderMUI.jsx";
-import { FaFemale } from 'react-icons/fa';
-import { FaMale } from 'react-icons/fa';
-import male from './assets/male.png';
-import female from './assets/female.png';
-import Box from '@mui/material/Box';
+import GenderSelection from "./components/genderSelectionMUI.jsx";
 
-const styles = {
-    appHeader: {
-        backgroundColor: 'darkgrey',
-        height: '200px',
-        padding: '10px',
-        color: 'antiquewhite',
-      },
-      
-      gender: {
-        padding: '10px',
-      },
-};
 
 const App = () => {
-    const select = [
-        {
-          name: "Male",
-          choice: "Choose if assigned Male at birth",
-          image: <img className='center' src={male} alt='male' style={{ width: '50px', height: '50px' }} />,
-        },
-        {
-          name: "Female",
-          choice: "Choose if assigned female at birth",
-          image:<img className='center' src={female} alt='female' style={{ width: '50px', height: '50px' }}/>,
-        }
-      ];
-    const name = "What was your gender assignment at birth?";
 
     return (
-        <>
-        <header style={styles.appHeader}>
-        <FaFemale />
-        <FaMale />
-        </header>
-        <Box sx={styles.gender}>
-        <Gender title={name} select={select} />
-        </Box>
-        </>
+        <div>
+          {/*Navigation Bar */}
+          <nav>
+            <ul>
+              <li>Feeback</li>
+              <li><GenderSelection /></li>
+            </ul>
+          </nav>
+          <main>
+            {/*Main Page Content */}
+
+          </main>
+        </div>
       );
 };
 export default App;
